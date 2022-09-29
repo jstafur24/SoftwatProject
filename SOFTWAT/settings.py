9a86cd2c7bce8dc898c47d7f3a6e4f02135578cb
jstafur24 <jstafur24@misena.edu.co>
2022-09-29 15:33:28 -0500

    .
    .

--- SOFTWAT/settings.py
+++ SOFTWAT/settings.py
@@ -159,9 +159,12 @@ STATICFILES_DIRS = (
 
 
 STATIC_TMP = os.path.join(BASE_DIR,"static")
-STATIC_ROOT = os.path.join(BASE_DIR,"static")
-os.makedirs(STATIC_ROOT, exist_ok=True)
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = "media/"
+MEDIAFILES_DIRS = (
+    
+    os.path.join(BASE_DIR,"media/"),
+
+)
