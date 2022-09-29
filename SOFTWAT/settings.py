19fdaedef6e5487181cd03b9135df37f04510e7a
jstafur24 <jstafur24@misena.edu.co>
2022-09-29 15:23:49 -0500

    Update settings.py

--- SOFTWAT/settings.py
+++ SOFTWAT/settings.py
@@ -129,19 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATIC_TMP = os.path.join(BASE_DIR,"static")
-STATIC_ROOT = os.path.join(BASE_DIR,"static")
-os.makedirs(STATIC_ROOT, exist_ok=True)
 
 
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -161,3 +150,18 @@ EMAIL_USE_TLS = True # y se deja en true
 
 # https://myaccount.google.com/security 
 #para generar contraseñas
+
+#Despliegue
+STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+STATIC_TMP = os.path.join(BASE_DIR,"static")
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
